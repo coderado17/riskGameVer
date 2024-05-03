ac334e9be63bec20112a0dd939290f2d2a0b9beb
coderado17 <147054561+coderado17@users.noreply.github.com>
2024-05-03 14:46:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,8 +16,8 @@ void printDice(int diceNo, int xPoint, int yPoint);
 int main(){
     joystick.init();
     screen.init(LPH7366_1);        //initialise for LPH7366-1 LCD (Options are LPH7366_1 and LPH7366_6)
-    screen.setContrast(1.0);      //set contrast to 55%
-    screen.setBrightness(0.1);     //set brightness to 50% (utilises the PWM)
+    screen.setContrast(1.0);      //set contrast to 100%
+    screen.setBrightness(0.1);     //set brightness to 10% (utilises the PWM)
     int mvm = 0;
     int curr = 0;
     int att = 8;
@@ -27,29 +27,9 @@ int main(){
     int i = 1;
 
     while (1) {
-
-        //game.displayDecisions(screen, joystick, &att, &def);
-        /*screen.clear();
-        game.diceThrow(screen, 1, true);
-        game.diceThrow(screen, 2, true);
-        game.diceThrow(screen, 3, true); 
-        game.diceThrow(screen, 1, false);
-        game.diceThrow(screen, 2, false);
-        screen.printString("VS", 35, 2);
-        screen.refresh();*/
-
-        //game.test(screen, 2, 1);
-       /* game.testArray();
-        printf("\n");
-        ThisThread::sleep_for(150000ms); */
-
-        //printf("Attack from: %d vs Defence from: %d", att, def);
-        //game.displayDecisions(screen, joystick, &att, &def);
-        game.playRISK(screen, joystick);
         
-       /* if (force.read() > 0.7) printf("Pressed!");
-        else printf("Released!"); */
-
-    ThisThread::sleep_for(500ms);
+        game.playRISK(screen, joystick);
+        ThisThread::sleep_for(500ms);
+        
     }
 }
